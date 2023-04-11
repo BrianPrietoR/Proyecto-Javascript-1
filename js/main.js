2f4aa25b2d9352f831433df33cb5ebab62299caa
Brian Prieto <prietob243@gmail.com>
2023-04-10 20:32:25 -0500

    cambiar colores de temas completado

--- js/main.js
+++ js/main.js
@@ -53,4 +53,20 @@ $(document).ready(function () {
         $("#posts").append(post);
     });
 
+    //selector de tema
+    var theme = $("#theme");
+    $("#to-green").click(function(){
+        theme.attr("href", "css/green.css")
+    });
+
+    $("#to-red").click(function(){
+        theme.attr("href", "css/red.css")
+    });
+
+    $("#to-blue").click(function(){
+        theme.attr("href", "css/blue.css")
+    });
+
+    //fin de selector
+
 });
